858feae82b270817910521ee1058ad9c477e9c91
Milosch Meriac <milosch@meriac.com>
2015-06-13 21:55:46 +0100

    added quotes

---
+++ firmware/blinkenbag/src/quotes.h
@@ -0,0 +1,80 @@
+#ifndef __QUOTES_H__
+#define __QUOTES_H__
+
+
+const char* g_quotes[] = {
+	/* Douglas Adams: Hitchhikers guide to the Galaxy */
+
+	"Far out in the uncharted backwaters of the unfashionable"
+	" end of the western spiral arm of the Galaxy lies a smal"
+	"l unregarded yellow sun. Orbiting this at a distance of "
+	"roughly ninety-two million miles is an utterly insignifi"
+	"cant little blue-green planet whose ape-descended life f"
+	"orms are so amazingly primitive that they still think di"
+	"gital watches are a pretty neat idea.",
+
+
+	/* Catcher in the Rye */
+
+	"I thought what I'd do was, I'd pretend I was one of thos"
+	"e deaf-mutes.",
+
+
+	/* Fight Club */
+
+	"You're not your job. You're not how much money you have "
+	"in the bank. You're not the car you drive. You're not th"
+	"e contents of your wallet. You're not your fucking khaki"
+	"s. You're the all-singing, all-dancing crap of the world"
+	".",
+
+	"This is your life and it's ending one minute at a time.",
+
+	"On a long enough timeline, the survival rate for everyo"
+	"ne drops to zero.",
+
+	"When you have insomnia, you're never really asleep... a"
+	"nd you're never really awake.",
+
+	"And then, something happened. I let go. Lost in oblivio"
+	"n. Dark and silent and complete. I found freedom. Losin"
+	"g all hope was freedom.",
+
+	"Without pain, without sacrifice, we would have nothing."
+	" Like the first monkey shot into space.",
+
+	"With insomnia, nothing's real. Everything's far away. E"
+	"verything's a copy of a copy of a copy.",
+
+	"When deep space exploration ramps up, it'll be the corp"
+	"orations that name everything, the IBM Stellar Sphere, "
+	"the Microsoft Galaxy, Planet Starbucks.",
+
+
+	/* Bokonon */
+
+	"As Bokonon says: \"Peculiar travel suggestions are danc"
+	"ing lessons from God.",
+
+	"If you wish to study a granfalloon, just remove the ski"
+	"n of a toy balloon.",
+
+
+	/* Sasha Yuvchenko - Nightshift at Chernobyl power plant
+	 * on April 25 1986 */
+
+	"There was a heavy thud. A couple of seconds later, I fe"
+	"lt a wave come through the room. The thick concrete wal"
+	"ls were bent like rubber. I thought war had broken out."
+	" We started to look for Khodemchuk but he had been by t"
+	"he pumps and had been vaporised. Steam wrapped around e"
+	"verything; it was dark and there was a horrible hissing"
+	" noise. There was no ceiling, only sky; a sky full of s"
+	"tars. A stream of ionising radiation was shooting starw"
+	"ards, like a laser beam. I remember thinking how beauti"
+	"ful it was.",
+};
+
+#define QUOTE_COUNT ((int)(sizeof(g_quotes)/sizeof(g_quotes[0])))
+
+#endif/*__QUOTES_H__*/
